af8c12f3aaa4a78b182129617e403fa9f52e380d
Harry <you@example.com>
2024-12-13 20:32:25 -0500

    base player and rail path now use base c++ classes.

---
+++ Source/K7_Style_Multiplayer/cRail_Path.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerCapsule.h"
+#include "cRail_Path.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class K7_STYLE_MULTIPLAYER_API AcRail_Path : public ATriggerCapsule
+{
+	GENERATED_BODY()
+	
+};
